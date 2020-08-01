e891c432f472ec57694aead7689360aa12fd38ac
lredd1 <l.redd0788@gmail.com>
2020-07-31 21:02:53 -0700

    resume app

---
+++ precache-manifest.d3dd0df913e770465a79269866b17ff1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107ecc659fd6818ec0d38a44389d3d40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90663b106deb859944e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "23ab85eb8e85325bc630",
+    "url": "/static/css/main.de1392dc.chunk.css"
+  },
+  {
+    "revision": "c90663b106deb859944e",
+    "url": "/static/js/2.44a0c16e.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.44a0c16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ab85eb8e85325bc630",
+    "url": "/static/js/main.1b9920d3.chunk.js"
+  },
+  {
+    "revision": "bbd090f530377a02689d",
+    "url": "/static/js/runtime-main.fc984738.js"
+  },
+  {
+    "revision": "e4cfb0d927cd4ce34712c2fe1835e6da",
+    "url": "/static/media/black-fist.e4cfb0d9.svg"
+  }
+]);
